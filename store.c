1b2aedba2cbc012302dae7cb52f0ec887675a9e2
couchsp <vlg605@kyonggi.ac.kr>
2024-03-27 12:46:07 +0900

    test commit

---
+++ store.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+
+int main(void) {
+	FILE* fp = NULL;
+
+	fopen_s(&fp, "alphabet.txt", "w");
+	if (fp = NULL) {
+		fprintf(stderr, "파일 alphabet.txt를 열 수 없습니다.\n");
+		exit(1);
+	}
+
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		fputc(c, fp);
+
+	fclose(fp);
+	return 0;
+}
